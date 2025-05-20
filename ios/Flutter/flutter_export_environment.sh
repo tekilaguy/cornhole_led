323391b48a03508f61649f7454e8140c026ff221
John Laws <john.laws@bluetrace.net>
2025-05-20 15:48:41 -0500

    IOS reload

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=c:\flutter"
-export "FLUTTER_APPLICATION_PATH=G:\Dropbox\Documents\Bluetrace Entertainment\Cornhole Boards\App\cornhole_led"
+export "FLUTTER_ROOT=/Users/tekilaguy/development/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/tekilaguy/Documents/cornhole_led"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
-export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.10"
 export "FLUTTER_BUILD_NUMBER=1.0.10"
